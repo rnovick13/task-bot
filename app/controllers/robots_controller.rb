1e21f5354a4966b5d8290ec306ca9c20c4181b7e
rnovick13 <rnovick13@gmail.com>
2019-11-09 11:28:07 -0500

    create created tested and functional

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -8,5 +8,20 @@ class RobotsController < ApplicationController
       @robot = Robot.find(params[:id])
   end
 
+  def new
+    @robot = Robot.new
+  end
+
+  def create
+    @robot = Robot.create(robot_params)
+
+    redirect_to robot_path(@robot)
+  end
+
+private
+  def robot_params
+    params.require(:robot).permit(:name, :slogan)
+  end
+
 
 end
